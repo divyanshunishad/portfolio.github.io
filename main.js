1d921e8232ad5baad3a9a2d0661c35926f8accb0
Divyanshu  Nishad <70854919+divyanshunishad@users.noreply.github.com>
2023-02-23 02:21:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mysidenav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mysidenav").style.width = "0";
+  }
